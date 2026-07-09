b2eb995ce9f37560efd8b5a9fc705ad5903fdb4e
Victoria <galaxydefender@live.com>
2026-07-09 23:39:01 +0200

    added chapter 2

--- compile.sh
+++ compile.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Clean temporary files before compiling
 rm -f *.aux *.bbl *.bcf *.blg *.log *.out *.run.xml *.toc *.lof *.lot *.lol *.synctex.gz
 
-# Compile
 pdflatex main.tex
 biber main
 pdflatex main.tex
